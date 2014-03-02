e56111ba3d1022eca5eae68792b86dd865cac0ae
adjigesangg04 <adjitechdeck@gmail.com>
2014-03-02 17:11:10 +0630

    profil

--- application/views/front/profil.php
+++ application/views/front/profil.php
@@ -2,7 +2,7 @@
 	<section>
 		<div class="header">
 			<div class="sampul">
-				  <img class="media-object img-circle avatar" src="assets/img/blog/avatar.jpg" alt="miniature-comment" width="80"><br>
+				  <img class="media-object img-circle avatar" src="assets/foto/a.jpg" alt="miniature-comment" width="80"><br>
 				  <label style="font-size:14pt;font-weight:bold;">Adji Gesang</label><br>
 				  jadbsajbdasdhasbfhbasc asbasccac ahcau caiu cahsdklfhushfdsf sdfhsduhfs fsd fsdu fusd f dsuf dsuhf ashdbhsabd as dasdasdh asd asd asg dashd sad as dashdasd<br>
 				  Location :
@@ -31,15 +31,54 @@
 		<div class="post">
 			<label style="font-size:15pt;">Post</label><hr>
 			
-			<table border="1">
-				<tr>
-					<td>asd</td>
-				</tr>
-				<tr>
-					<td>fdg</td>
-					<td rowspan="2">yth</td>
-				</tr>
-			</table>
+			<div class="media">
+				<a class="pull-left" href="#">
+				  <img class="media-object img-circle ava" src="assets/foto/a.jpg" alt="miniature-comment" width="48">
+				</a>
+				<div class="media-body">
+					<label style="font-weight:bold;">Adji Gesang</label>
+					<small style="color:#b3b3b3;">@adjigesangg</small><br>
+				  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+					<div style="text-align:right;">
+					  <button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-share-alt"></span> Reply</button>
+					  <button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-star"></span> Favourite</button>
+					  <button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-trash"></span> Delete</button>
+					</div>
+				</div>
+				<hr>
+			</div>
+			<div class="media">
+				<a class="pull-left" href="#">
+				  <img class="media-object img-circle ava" src="assets/foto/a.jpg" alt="miniature-comment">
+				</a>
+				<div class="media-body">
+					<label style="font-weight:bold;">Adji Gesang</label>
+					<small style="color:#b3b3b3;">@adjigesangg</small><br>
+				  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+					<div style="text-align:right;">
+					  <button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-share-alt"></span> Reply</button>
+					  <button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-star"></span> Favourite</button>
+					  <button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-trash"></span> Delete</button>
+					</div>
+				</div>
+				<hr>
+			</div>
+			<div class="media">
+				<a class="pull-left" href="#">
+				  <img class="media-object img-circle ava" src="assets/foto/a.jpg" alt="miniature-comment" width="48">
+				</a>
+				<div class="media-body">
+					<label style="font-weight:bold;">Adji Gesang</label>
+					<small style="color:#b3b3b3;">@adjigesangg</small><br>
+				  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+					<div style="text-align:right;">
+					  <button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-share-alt"></span> Reply</button>
+					  <button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-star"></span> Favourite</button>
+					  <button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-trash"></span> Delete</button>
+					</div>
+				</div>
+				<hr>
+			</div>
 		</div>
 	</section>
 </div>
